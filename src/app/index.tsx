d2f786b39377eb6f89ddb3f69066d88536e7a03f
evgeniaStep <stepanova.e49@wb.ru>
2025-01-18 21:05:24 +0300

    base

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+// import clsx from 'clsx';
+// import { useState } from 'react';
+// import styles from './app.module.scss';
+
+export const App = () => {
+	return (
+		<div className='page'></div>
+	);
+};
